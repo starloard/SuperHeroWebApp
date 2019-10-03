1a70422148214c04f5771d9c488bd9fa760cba1f
starloard <abhisuryawanshi99@gmail.com>
2019-10-03 21:40:55 +0530

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, NgModule } from '@angular/core';
+import { CommonModule } from "@angular/common";
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Captain America' },
+  { id: 12, name: 'Black Widow' },
+  { id: 13, name: 'Hulk' },
+  { id: 14, name: 'Black Panther' },
+  { id: 15, name: 'Ant Man' },
+  { id: 16, name: 'Spider-Man' },
+  { id: 17, name: 'Thor' },
+  { id: 18, name: 'Dr Strange' },
+  { id: 19, name: 'Starlord' },
+  { id: 20, name: 'Captain Marvel' }
+];
